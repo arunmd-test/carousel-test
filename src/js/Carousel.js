5bfaf497147bd633810d46abc1941f46f92773f0
ArunMichaelDsouza <amdsouza92@gmail.com>
2018-12-28 03:54:29 +0530

    Carousel strip image display logic with placement calculations added

--- src/js/Carousel.js
+++ src/js/Carousel.js
@@ -19,6 +19,78 @@ class Carousel {
         this.carousel = null; // Reference to the carousel widget element
         this.activeIndex = Math.ceil(MAX_IMAGES / 2); // Index of the active item, to be centered in the middle
         this.stripItems = []; // Reference to the carousel strip items
+        this.allPlacementIndexes = []; // Reference to all carousel placement indexes
+    }
+
+    setPlacementImages() {
+        let images = [];
+
+        for (let i = 0; i < this.images.length; i++) {
+            for (let j = 0; j < this.allPlacementIndexes.length; j++) {
+                if (i === this.allPlacementIndexes[j]) {
+                    images.push(this.images[i]);
+                }
+            }
+        }
+
+        for (let i = 0; i < this.stripItems.length; i++) {
+            const img = this.stripItems[i].children[0];
+            const title = this.stripItems[i].children[1];
+
+            img.src = images[i] ? images[i].url : '';
+            img.alt = images[i] ? images[i].title : '';
+            title.innerText = img.alt;
+        }
+
+        return;
+    }
+
+    getStripPlacementIndexes(type, activeImageIndex, placements) {
+        let placementIndexes = [];
+
+        if (type === 'left') {
+            for (let i = activeImageIndex - 1; i >= (activeImageIndex - placements.left); i--) {
+                placementIndexes.push(i);
+            }
+        } else if (type === 'right') {
+            for (let i = activeImageIndex + 1; i <= (activeImageIndex + placements.right); i++) {
+                placementIndexes.push(i);
+            }
+        }
+
+        return placementIndexes.sort();
+    }
+
+    getStripPlacementsCount() {
+        let placements = { left: 0, right: 0 };
+
+        for (let i = 1; i <= this.maxImages; i++) {
+            if (i < this.activeIndex) {
+                placements.left++;
+            } else if (i > this.activeIndex) {
+                placements.right++;
+            }
+        }
+
+        return placements;
+    }
+
+    displayImages() {
+        const placements = this.getStripPlacementsCount();
+        let activeImageIndex;
+
+        for (let i = 0; i < this.images.length; i++) {
+            if (this.images[i].active) {
+                activeImageIndex = i;
+                break;
+            }
+        }
+
+        const leftPlacementIndexes = this.getStripPlacementIndexes('left', activeImageIndex, placements);
+        const rightPlacementIndexes = this.getStripPlacementIndexes('right', activeImageIndex, placements);
+        this.allPlacementIndexes = leftPlacementIndexes.concat(activeImageIndex, rightPlacementIndexes);
+
+        return this.setPlacementImages();
     }
 
     createItem(isActive) {
@@ -42,6 +114,7 @@ class Carousel {
             this.stripItems.push(item);
             strip.appendChild(item);
         }
+        this.displayImages();
 
         return strip;
     }
