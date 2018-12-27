5ff729eb1f08398215b6528ef54145b88581a1d5
ArunMichaelDsouza <amdsouza92@gmail.com>
2018-12-28 02:54:16 +0530

    Webpack config created for module bundling and transpilation

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+// Webpack bundling config
+
+const path = require('path');
+const config = {
+    entry: './src/js/index.js',
+    output: {
+        path: path.join(__dirname, 'build'),
+        filename: 'carousel.min.js'
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /.js?$/,
+                loader: 'babel-loader'
+            }
+        ]
+    }
+};
+
+module.exports = config;
