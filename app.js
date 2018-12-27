d9991eccc11afa663769297a556c3fdc22c11411
ArunMichaelDsouza <amdsouza92@gmail.com>
2018-12-28 02:45:27 +0530

    Carousel initialisation method added to app module

--- app.js
+++ app.js
@@ -3,16 +3,31 @@
 */
 
 var app = (function (w) {
-    var imageUrl = 'https://pixabay.com/api/?key=9656065-a4094594c34f9ac14c7fc4c39&q=beautiful+landscape&image_type=photo';
+    var imageUrl = 'https://pixabay.com/api/?key=9656065-a4094594c34f9ac14c7fc4c39&q=beautiful+landscape&image_type=photo',
+        carouselElement = document.getElementById('carousel'),
+        carouselTitle = 'Carousel Test',
+        activeItem = 5;
 
     return {
+        processImages: function (images) {
+            return images.map(function (image, index) {
+                return {
+                    url: image.webformatURL,
+                    title: image.user,
+                    active: index === activeItem ? true : false
+                };
+            });
+        },
+        initCarousel: function (images) {
+            return new Carousel(carouselElement, carouselTitle, this.processImages(images)).init();
+        },
         fetchImages: function () {
             var xhr = new w.XMLHttpRequest();
 
             xhr.open('GET', imageUrl);
             xhr.onreadystatechange = function () {
                 if (xhr.readyState == 4 && xhr.status == 200) {
-                    console.log(JSON.parse(xhr.responseText).hits);
+                    return app.initCarousel(JSON.parse(xhr.responseText).hits);
                 }
             }
             xhr.send();
