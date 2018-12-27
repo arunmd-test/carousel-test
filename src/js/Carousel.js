aa3a6c4b45638539767a39a620aff73e4c812789
ArunMichaelDsouza <amdsouza92@gmail.com>
2018-12-28 03:19:22 +0530

    Carousel strip + strip items creation methods created

--- src/js/Carousel.js
+++ src/js/Carousel.js
@@ -3,7 +3,7 @@
 */
 
 import { createElement } from './utils';
-import { MAX_IMAGES, CAROUSEL_CLASS, HEADER_CLASS } from './constants';
+import { MAX_IMAGES, CAROUSEL_CLASS, HEADER_CLASS, STRIP_CLASS, ITEM_CLASS, ITEM_ACTIVE_CLASS, IMG_CLASS, IMG_TITLE_CLASS } from './constants';
 
 
 class Carousel {
@@ -17,6 +17,33 @@ class Carousel {
 
         this.maxImages = MAX_IMAGES; // Max number of images to be displayed in the carousel strip
         this.carousel = null; // Reference to the carousel widget element
+        this.activeIndex = Math.ceil(MAX_IMAGES / 2); // Index of the active item, to be centered in the middle
+        this.stripItems = []; // Reference to the carousel strip items
+    }
+
+    createItem(isActive) {
+        const item = createElement('li', isActive ? ITEM_ACTIVE_CLASS : ITEM_CLASS, '', { 'role': 'listitem' });
+        const img = createElement('img', IMG_CLASS);
+        const title = createElement('h2', IMG_TITLE_CLASS);
+
+        item.style.width = `${100 / this.maxImages}%`;
+        item.append(img, title);
+
+        return item;
+    }
+
+    createStrip() {
+        const strip = createElement('ul', STRIP_CLASS, '', { 'role': 'list' });
+
+        for (let i = 1; i <= this.maxImages; i++) {
+            const isActive = i === this.activeIndex;
+            const item = this.createItem(isActive);
+
+            this.stripItems.push(item);
+            strip.appendChild(item);
+        }
+
+        return strip;
     }
 
     createHeader() {
@@ -25,7 +52,7 @@ class Carousel {
 
     createCarousel() {
         const carousel = createElement('div', CAROUSEL_CLASS, '', { 'aria-label': this.title, 'role': 'region' });
-        carousel.append(this.createHeader());
+        carousel.append(this.createHeader(), this.createStrip());
 
         return this.carousel = carousel;
     };
--- src/js/constants.js
+++ src/js/constants.js
@@ -5,5 +5,9 @@
 const MAX_IMAGES = 6;
 const CAROUSEL_CLASS = 'carousel';
 const HEADER_CLASS = 'carousel-header';
+const ITEM_CLASS = 'carousel-item';
+const ITEM_ACTIVE_CLASS = 'carousel-item-active';
+const IMG_CLASS = 'carousel-image';
+const IMG_TITLE_CLASS = 'carousel-image-title';
 
-export { MAX_IMAGES, CAROUSEL_CLASS, HEADER_CLASS };
+export { MAX_IMAGES, CAROUSEL_CLASS, HEADER_CLASS, ITEM_CLASS, ITEM_ACTIVE_CLASS, IMG_CLASS, IMG_TITLE_CLASS };
