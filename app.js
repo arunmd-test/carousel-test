477ecf67bb10040e6c8e469d05845e247aee3491
ArunMichaelDsouza <amdsouza92@gmail.com>
2018-12-28 02:47:51 +0530

    Loader added to demo page for better UX

--- app.js
+++ app.js
@@ -5,6 +5,7 @@
 var app = (function (w) {
     var imageUrl = 'https://pixabay.com/api/?key=9656065-a4094594c34f9ac14c7fc4c39&q=beautiful+landscape&image_type=photo',
         carouselElement = document.getElementById('carousel'),
+        loaderElement = document.getElementById('loader'),
         carouselTitle = 'Carousel Test',
         activeItem = 5;
 
@@ -19,6 +20,7 @@ var app = (function (w) {
             });
         },
         initCarousel: function (images) {
+            loaderElement.style.display = 'none';
             return new Carousel(carouselElement, carouselTitle, this.processImages(images)).init();
         },
         fetchImages: function () {
